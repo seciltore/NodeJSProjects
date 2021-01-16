b49c15718c6dd118085ec1a37bee367f4620f893
Cecil T <seciltore.dc@gmail.com>
2021-01-15 20:38:45 -0500

    NodeJS

--- Node-Server/app-server.js
+++ Node-Server/app-server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+//const http = require('http');
 
 // function rqlistener(req, res){
 
@@ -9,10 +9,56 @@ const http = require('http');
 
 // });
 
-const server = http.createServer((req, res) => {
-    console.log(req);
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method, req.headers);
+//     //process.exit();
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<html>');
+//     res.write('<head><title>My First Page</title></head>');
+//     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+//     res.write('</html>');
+//     res.end;
+// });
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res)=>{
+    const url = req.url;
+    const method = req.method;
+    if(url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter mesage</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+        res.write('</html>');
+        return res.end;
+    }
+    if(url==='/message' && method ==='POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () =>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+            // console.log(parsedBody);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end;
+  
 });
 
 server.listen(3000);
+
 
 
